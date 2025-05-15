9acbdab9923a39754c4af0932e63cc46c0d5bd3c
h28a3 <at.h.s_24328@iCloud.com>
2025-05-15 23:04:39 +0900

    Create a.py

---
+++ 25-5-10/src/a.py
@@ -0,0 +1,12 @@
+r, x = map(int, input().split())
+if x==1:
+    if r>=1600 and r<3000:
+        print("Yes")
+    else:
+        print("No")
+
+else:
+    if r>=1200 and r<2400:
+        print("Yes")
+    else:
+        print("No")
